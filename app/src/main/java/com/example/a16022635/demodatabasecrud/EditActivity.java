03b0dd639e08f85640dcaac6d4683621db8121f1
teozhirong1998 <16022635@myrp.edu.sg>
2018-05-15 22:01:40 +0800

    Initial commit

---
+++ app/src/main/java/com/example/a16022635/demodatabasecrud/EditActivity.java
@@ -0,0 +1,59 @@
+package com.example.a16022635.demodatabasecrud;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class EditActivity extends AppCompatActivity {
+
+    TextView tvID;
+    EditText etContent;
+    Button btnUpdate, btnDelete;
+    Note data;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit);
+
+        tvID = (TextView) findViewById(R.id.tvID);
+        etContent = (EditText) findViewById(R.id.etContent);
+        btnUpdate = (Button) findViewById(R.id.btnUpdate);
+        btnDelete = (Button) findViewById(R.id.btnDelete);
+
+        Intent i = getIntent();
+        data = (Note) i.getSerializableExtra("data");
+
+        tvID.setText("ID: " + data.getId());
+        etContent.setText(data.getNoteContent());
+
+        btnUpdate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DBHelper dbh = new DBHelper(EditActivity.this);
+                data.setNoteContent(etContent.getText().toString());
+                dbh.updateNote(data);
+                dbh.close();
+                setResult(RESULT_OK);
+                finish();
+            }
+        });
+
+        btnDelete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DBHelper dbh = new DBHelper(EditActivity.this);
+                dbh.deleteNote(data.getId());
+                dbh.close();
+
+                setResult(RESULT_OK);
+                finish();
+            }
+        });
+
+    }
+}
---
+++ app/src/main/java/com/example/a16022635/demodatabasecrud/Note.java
@@ -0,0 +1,29 @@
+package com.example.a16022635.demodatabasecrud;
+
+import java.io.Serializable;
+
+public class Note implements Serializable {
+
+    private int id;
+    private String noteContent;
+
+    public Note(int id, String noteContent) {
+        this.id = id;
+        this.noteContent = noteContent;
+    }
+
+    @Override
+    public String toString() {
+        return "ID:" + id + ", " + noteContent;
+    }
+
+    public int getId() { return id; }
+
+    public String getNoteContent() {
+        return noteContent;
+    }
+
+    public void setNoteContent(String noteContent) {
+        this.noteContent = noteContent;
+    }
+}
